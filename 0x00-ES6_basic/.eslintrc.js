0d305d1022f66f92c3d4b9daf896e900abe8578f
augustinos <auguststinos.nabil@gmail.com>
2024-08-05 18:38:11 +0000

    fix

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,19 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    // Add more rules as needed
+  },
+};
