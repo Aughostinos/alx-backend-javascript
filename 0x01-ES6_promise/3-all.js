73673102b31942af7c8603622492467314e79327
Aughostinos <augustinos.nabil@gmail.com>
2024-08-07 22:56:19 +0300

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photo, user] = results;
-      console.log(`${photo.body} ${user.firstname} ${user.lastname}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
