3b8a938162bbe297a5862fdac047a22414d92fdc
Aughostinos <augustinos.nabil@gmail.com>
2024-08-09 04:24:17 +0300

    task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,8 +1,16 @@
-class Car{
-    constructor(brand, motor, color){
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
-    
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
 }
+export default Car;
