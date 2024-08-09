36370390dcf889d6f18d2192185f1bf5a999d0bb
Aughostinos <augustinos.nabil@gmail.com>
2024-08-09 05:07:00 +0300

    fix 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,13 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor();
+    const NewCar = Object.getPrototypeOf(this).constructor;
+    return new NewCar(this._brand, this._motor, this._color);
   }
 }
 export default Car;
