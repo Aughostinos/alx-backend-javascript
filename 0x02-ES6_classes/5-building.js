b0c0c64afa78bb0679253ca9aec28a127f20aedf
Aughostinos <augustinos.nabil@gmail.com>
2024-08-09 04:38:32 +0300

    fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 class Building {
   constructor(sqft) {
-    if(new.target === Building){
-        throw new Error('Cannot instantiate class')
-    }
     this._sqft = sqft;
   }
 
@@ -12,9 +9,7 @@ class Building {
   }
 
   evacuationWarningMessage() {
-    if (this instanceof Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 }
 export default Building;
