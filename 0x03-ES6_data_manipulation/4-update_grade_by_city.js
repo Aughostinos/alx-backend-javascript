ef035c0d8b172c2129ba482acc013f5697c1827a
Aughostinos <augustinos.nabil@gmail.com>
2024-08-13 16:54:56 +0300

    task4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  return getListStudents
+    .filters((student) => student.location === city)
+    .map((student) => {
+      const updatedGrades = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: updatedGrades ? updatedGrades.grade : 'N/A',
+      };
+    });
+}
+export default updateStudentGradeByCity;
