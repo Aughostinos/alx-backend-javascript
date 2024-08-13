e4d103710f7c067f6f7eee5e4471c3e786d2f428
Aughostinos <augustinos.nabil@gmail.com>
2024-08-14 01:26:39 +0300

    fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
+  const dataView = new DataView(buffer);
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
-  int8View[position] = value;
-  return int8View;
+  dataView.setInt8(position, value);
+  return dataView;
 }
 
 export default createInt8TypedArray;
