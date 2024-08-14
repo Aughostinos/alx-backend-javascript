c7ee8512fd85c52fda540b89d5aff0a01bd31efa
Aughostinos <augustinos.nabil@gmail.com>
2024-08-14 05:02:39 +0300

    Task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,15 @@
 export const myWeakMap = new WeakMap();
 
-function queryAPI(endPoint) {
-  if (!myWeakMap.has(endPoint)) {
+function queryAPI(endPoint){
+  if(!myWeakMap.has(endPoint)) {
     myWeakMap.set(endPoint, 0);
   }
 
-  const count = myWeakMap.get(endPoint) + 1;
+  let count = myWeakMap.get(endPoint) + 1;
   myWeakMap.set(endPoint, count);
 
   if (count >= 5) {
-    throw new Error('Endpoint load is high');
+    throw new Error('Endpoint load is high')
   }
 }
 export default queryAPI;
