95a0ba2e0ecf818b493b8b7ff0400ca3d1b9c780
Aughostinos <augustinos.nabil@gmail.com>
2024-10-01 03:55:32 +0300

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?\n');
 
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  if (process.stdin.isTTY) {
-    process.exit();
-  }
-});
-
-process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log("This important software is now closing");
+  process.exit();
 });
