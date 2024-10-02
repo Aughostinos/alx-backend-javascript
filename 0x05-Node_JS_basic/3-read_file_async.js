c9c85087870b1bcd9d09590aae87c0939a43a5f4
Aughostinos <augustinos.nabil@gmail.com>
2024-10-02 03:59:42 +0300

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.trim().split('\n');
+        const header = lines[0].split(',');
+        const students = lines.slice(1);
+        const fieldStudents = {};
+        let totalStudents = 0;
+
+        for (const line of students) {
+          if (line.trim() !== '') {
+            const studentData = line.split(',');
+            if (studentData.length === header.length) {
+              const student = {};
+              for (let i = 0; i < header.length; i += 1) {
+                student[header[i].trim()] = studentData[i].trim();
+              }
+              const field = student.field;
+              if (!fieldStudents[field]) {
+                fieldStudents[field] = [];
+              }
+              fieldStudents[field].push(student.firstname);
+              totalStudents += 1;
+            }
+          }
+        }
+
+        console.log(`Number of students: ${totalStudents}`);
+        Object.keys(fieldStudents).forEach((field) => {
+          const list = fieldStudents[field];
+          console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+        });
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
