85c8e12dfcad92ee7bd54e81aae53e96fbef472f
Aughostinos <augustinos.nabil@gmail.com>
2024-10-02 04:17:07 +0300

    task 6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//port
+const port = 1245;
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+    console.log(`Express server running on http://localhost:${port}/`)
+});
