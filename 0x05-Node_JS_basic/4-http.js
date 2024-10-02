5d37bc0090088272e48eeb1094a05abe9814476f
Aughostinos <augustinos.nabil@gmail.com>
2024-10-02 04:11:59 +0300

    fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -8,7 +8,8 @@ const app = http.createServer((req, res) => {
 });
 
 // Listen on port 1245
-server.listen(1245, () => {
+app.listen(1245, () => {
   console.log('Server running at http://127.0.0.1:1245/');
 });
+
 module.exports = app;
