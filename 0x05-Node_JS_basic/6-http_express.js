2225b6407f35910b0a87b909a6e9cf01c47b0fda
augustinos <augustinos.nabil@gmail.com>
2024-10-02 01:27:32 +0000

    fixing code style

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,15 +1,16 @@
 const express = require('express');
+
 const app = express();
 
-//port
+// port
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-    console.log(`Express server running on http://localhost:${port}/`)
+  console.log(`Express server running on http://localhost:${port}/`);
 });
 
 module.exports = app;
