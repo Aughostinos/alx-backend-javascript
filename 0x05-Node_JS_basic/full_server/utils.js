39fede2d705570a3aa5f5143f1e6e687dd80fbf0
Aughostinos <augustinos.nabil@gmail.com>
2024-10-02 05:46:55 +0300

    fix

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,35 +1,34 @@
 // full_server/utils.js
-import fs from 'fs';
+import fs from 'fs/promises';
 
-export function readDatabase(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(Error('Cannot load the database'));
-      } else {
-        const lines = data.trim().split('\n');
-        const header = lines[0].split(',');
-        const students = lines.slice(1);
-        const fieldStudents = {};
+async function readDatabase(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const lines = data.trim().split('\n');
+    const header = lines[0].split(',');
+    const students = lines.slice(1);
+    const fieldStudents = {};
 
-        for (const line of students) {
-          if (line.trim() !== '') {
-            const studentData = line.split(',');
-            if (studentData.length === header.length) {
-              const student = {};
-              for (let i = 0; i < header.length; i += 1) {
-                student[header[i].trim()] = studentData[i].trim();
-              }
-              const field = student.field;
-              if (!fieldStudents[field]) {
-                fieldStudents[field] = [];
-              }
-              fieldStudents[field].push(student.firstname);
-            }
+    for (const line of students) {
+      if (line.trim() !== '') {
+        const studentData = line.split(',');
+        if (studentData.length === header.length) {
+          const student = {};
+          for (let i = 0; i < header.length; i += 1) {
+            student[header[i].trim()] = studentData[i].trim();
           }
+          const field = student.field;
+          if (!fieldStudents[field]) {
+            fieldStudents[field] = [];
+          }
+          fieldStudents[field].push(student.firstname);
         }
-        resolve(fieldStudents);
       }
-    });
-  });
+    }
+    return fieldStudents;
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
+
+export default readDatabase;
