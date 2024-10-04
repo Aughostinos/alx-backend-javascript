13afcf048d9a2ed9e0a7bb3c9e8f85726aab23f9
Aughostinos <augustinos.nabil@gmail.com>
2024-10-04 05:22:02 +0300

    fix task6 test cases

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,29 +5,17 @@ describe('getPaymentTokenFromAPI', () => {
   it('should return { data: "Successful response from the API" } when success is true', (done) => {
     getPaymentTokenFromAPI(true)
       .then((response) => {
-        expect(response).to.be.an('object');
-        expect(response).to.have.property('data');
-        expect(response.data).to.equal('Successful response from the API');
-        done();
+        try {
+          expect(response).to.be.an('object');
+          expect(response).to.have.property('data');
+          expect(response.data).to.equal('Successful response from the API');
+          done();
+        } catch (error) {
+          done(error);
+        }
       })
       .catch((error) => {
         done(error);
-      });
-  });
-
-  it('should not resolve or reject when success is false', (done) => {
-    const timeout = setTimeout(() => {
-      done(new Error('Promise did not resolve or reject as expected.'));
-    }, 100);
-
-    getPaymentTokenFromAPI(false)
-      .then(() => {
-        clearTimeout(timeout);
-        done(new Error('Promise should not have resolved.'));
-      })
-      .catch(() => {
-        clearTimeout(timeout);
-        done(new Error('Promise should not have rejected.'));
       });
   });
 });
