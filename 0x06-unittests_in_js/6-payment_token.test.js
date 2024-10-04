3d9da82267104352d6a145075409d6e45dbf60aa
Aughostinos <augustinos.nabil@gmail.com>
2024-10-04 05:14:57 +0300

    task 6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return { data: "Successful response from the API" } when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.be.an('object');
+        expect(response).to.have.property('data');
+        expect(response.data).to.equal('Successful response from the API');
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+
+  it('should not resolve or reject when success is false', (done) => {
+    // Set a timeout to simulate the hanging promise
+    const timeout = setTimeout(() => {
+      done(new Error('Promise did not resolve or reject as expected.'));
+    }, 100); // 100ms timeout
+
+    getPaymentTokenFromAPI(false)
+      .then(() => {
+        clearTimeout(timeout);
+        done(new Error('Promise should not have resolved.'));
+      })
+      .catch(() => {
+        clearTimeout(timeout);
+        done(new Error('Promise should not have rejected.'));
+      });
+  });
+});
